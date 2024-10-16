4e90ba87b35da9b027aaadfdbfefa6ed494587e4
Erudite885 <erudite885@gmail.com>
2024-10-17 04:29:45 +1300

    port

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     host: "127.0.0.1", // Forces IPv4
-    port: 3000, // Change to a different port if desired
+    port: 8080, // Change to a different port if desired
     strictPort: true, // Ensures Vite fails if the port is already in use
   },
 });
